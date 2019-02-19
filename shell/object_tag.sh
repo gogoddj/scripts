f55c0845ac2c3d89cea95c17fd35f941c05e106d
opsonly <13271331235@163.com>
2019-02-19 10:20:51 +0800

    修改git tag事件脚本

--- shell/object_tag.sh
+++ shell/object_tag.sh
@@ -1,46 +1,77 @@
 #!/bin/bash
 
-giturl="/.git/config"
-webdir="/var/www/html/"
-tagdir="/data/html/"
-nginxdir="/etc/nginx/conf.d/"
-nginxexam="/data/scripts/shell/nginx.conf"
-
-project=$1
-branch=$2
+tagDir="/data/html/"
+webDir="/var/www/html/"
+gitConfig="/.git/config"
 
 
-function ittagurl() {
-    cd $webdir$project
-    tag=`git tag|sort -nr|head -n 1`
-    cd $tagdir$project && mkdir $tag
-    cd $tag
-    cp -a $webdir$project . && cd $project
-    git fetch && git checkout $tag
-    cp .env.produce .env
+
+EnjoyJsDe="src/config/environment.js.product"
+EnjoyJs="src/config/environment.js"
+
+
+
+
+nginxdir="/etc/nginx/conf.d"
+nginxexam="/WebUpdate/nginx.conf"
+function url_test() {
+    url="'https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxxxx'"
+    header="'Content-Type: application/json'"
+    msg="'{\"msgtype\": \"text\",\"text\": {\"content\":\"$1\"}}'"
+    #curl $url -H $header -d $msg
+    a='curl '$url' -H '$header' -d '$msg
+    eval $a
+}
+
+
+for i in `ls $webDir`;do
+
+
+  #进入原项目目录，获取tag标签
+  cd $webDir$i
+
+  for j in `git tag|sort -nr|head -5`;do
+    #进入tag项目目录
+    cd $tagDir$i
+
+    #创建标签目录并切换至目录
+    mkdir $j && cd $j
+
+
+    #复制项目目录至当前标签目录
+    cp -a $webDir$i .
+
+    cd $i && git fetch
+
+    if [[ $i == "EnjoyCarWeb" ]];then
+      break
+    fi
+
+    git checkout $j && cp .env.produce .env
     composer install
-    /usr/local/php7/bin/php artisan queue:restart
 
-    prodir=$tagdir$project/$tag/$project
+    #获取tag并替换.
+    tag=`git status|awk '/HEAD/{print $5}'`
     httptag=`echo $tag|tr '.' '_'`
-    httpurl=$project'-'$httptag'_tag'
+    httpurl='tag-'$i'-'$httptag
+    project=$tagDir$i/$tag/$i
+
+    #配置nginx配置文件
     servername=$httpurl'.51ucar.cn'
-    nginxconf=$nginxdir/$httpurl.conf
+    nginxconf=$nginxdir/$httpurl'.conf'
     cp $nginxexam $nginxconf
     sed -i -r "s@root (/[A-Za-z]+)+;@root $project/public;@g" $nginxconf
     sed -i -r "s@server_name _@server_name $servername@g" $nginxconf
-	sed -i -r "s@https://x.51ucar.cn@https://$servername@" $nginxconf
+    sed -i -r "s@https://x.51ucar.cn@https://$servername@" $nginxconf
 
-	if `sudo -u ucar nginx -t`;then
-        sudo -u ucar nginx -s reload
-        url_test 'tag:'$tag' \n测试连接:https://'$servername
+    #重载nginx配置文件
+    if `sudo -u ucar nginx -t`;then
+      sudo -u ucar nginx -s reload
+      url_test 'tag:'$tag' \n测试连接:https://'$servername
     else
-        url_test '分支名:'$2' \nnginx配置失败'
-        exit
+      url_test '分支名:'$2' \nnginx配置失败'
+      exit
     fi
+  done
 
-    cd $tagdir
-    deleteDir=`ls |sort -n|head -n 1`
-    rm -rf $deleteDir
-
-}
+done
